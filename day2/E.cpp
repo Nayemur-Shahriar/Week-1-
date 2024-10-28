5afdbe5acfdc2c8ca74c9dfd6602adcbd67ac766
Nayemur-Shahriar <159870495+Nayemur-Shahriar@users.noreply.github.com>
2024-10-28 22:16:53 +0600

    E.cpp

--- day2/E.cpp
+++ day2/E.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    int a,b,t;
+    cin>>a>>b>>t;
+    
+    double time=t+0.5;
+    
+    int cnt=time/a;
+    
+    
 
+cout<<cnt*b;
+      
+       
+
+    return 0;
+}
