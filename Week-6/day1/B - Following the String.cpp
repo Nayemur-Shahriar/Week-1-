b71dbc833308c56980f9b72a7c7a4273976ebee3
Nayemur-Shahriar <159870495+Nayemur-Shahriar@users.noreply.github.com>
2024-11-24 21:49:22 +0600

    Add files via upload

---
+++ Week-6/day1/B - Following the String.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        map<int,int>mp;
+
+        //
+
+        for(int i=0; i<n; i++)
+        {
+            cin>>a[i];
+
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            cout<<(char)(mp[a[i]]+97);
+            mp[a[i]]++;
+
+        }
+        cout<<endl;
+
+
+
+
+
+    }
+
+    return 0;
+}
+
