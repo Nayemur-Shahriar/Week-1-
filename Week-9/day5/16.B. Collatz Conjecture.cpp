0ade79ba01be79f25c7d5acc7e20b10465959690
Nayemur-Shahriar <159870495+Nayemur-Shahriar@users.noreply.github.com>
2024-12-26 23:08:55 +0600

    Add files via upload

---
+++ Week-9/day5/16.B. Collatz Conjecture.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y, k;
+        cin >> x >> y >> k;
+
+        while (x > 1)
+        {
+            int p = y - (x % y);
+            if (p >= k)
+            {
+                x += k;
+                k = 0;
+            }
+            else
+            {
+                k -= p;
+                x += p;
+            }
+
+            while (x % y == 0)
+            {
+                x /= y;
+            }
+
+            if (k <= 0) break;
+        }
+
+        if (k > 0)
+        {
+            k = k %(y - 1);
+            x += k;
+            while (x % y == 0)
+            {
+                x /= y;
+            }
+        }
+
+        cout << x << endl;
+    }
+
+    return 0;
+}
