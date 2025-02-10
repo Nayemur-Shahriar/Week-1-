8ea97d5ade17970b08c927bc6c2e8846efb611a0
Nayemur-Shahriar <159870495+Nayemur-Shahriar@users.noreply.github.com>
2025-02-10 21:21:34 +0600

    Update E - Robin Hood in Town.cpp

--- Week-14/day3/E - Robin Hood in Town.cpp
+++ Week-14/day3/E - Robin Hood in Town.cpp
@@ -1,33 +1,78 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-
 int main()
 {
 
-    int t;
-    cin >> t;
-    while (t--)
+    long long t;
+    cin>>t;
+    while(t--)
     {
-        int n;
-        cin >> n;
+        long long n;
+        cin>>n;
 
-        long long sum = 0;
-        vector<long long> v(n);
+        long long arr[n+10],sum=0;
 
-        for (auto &c : v) cin >> c, sum += c;
-
-        sort(v.begin(),v.end());
-
-        if (n < 3)
+        for(int i=1; i<=n; i++)
         {
-           cout <<"-1"<<endl;
-           break;
+            cin>>arr[i];
+            sum+=arr[i];
         }
-        cout << max(0LL,v[n/2]*2*n-sum+1) <<endl;
+
+        if(n==1 || n==2)
+        {
+            cout<<-1<<endl;
+            continue;
+        }
+
+        long long l=0,r=1e18,ans=-1;
+
+        while(l<=r)
+        {
+
+            long long mid = (l + r) / 2;
+
+            long long avg=(mid+sum+n-1)/n;
+            long long half;
+
+            if(avg%2==0)
+            {
+                half=avg/2;
+            }
+            else
+            {
+                half=(avg+1)/2;
+            }
+
+            long long cnt=0;
+
+            for(int i=1; i<=n; i++)
+            {
+                if(arr[i]<half)
+                {
+                    cnt++;
+                }
+            }
+
+            long long need=n/2;
+
+            if(need<cnt && n%2==0)
+            {
+                ans=mid;
+                r=mid-1;
+            }
+            else if(need+1<=cnt)
+            {
+                ans=mid;
+                r=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+
+        }
+        cout<<ans<<endl;
+
     }
-
-
     return 0;
 }
