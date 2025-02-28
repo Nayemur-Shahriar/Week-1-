14a20b732deda19cd00c492d589991a449a1ab0d
Nayemur-Shahriar <159870495+Nayemur-Shahriar@users.noreply.github.com>
2025-02-28 23:24:12 +0600

    Update B - Turtle Math Fast Three Task.cpp

--- Week-16/day1/B - Turtle Math Fast Three Task.cpp
+++ Week-16/day1/B - Turtle Math Fast Three Task.cpp
@@ -1,55 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 #define ll long long
-
 
 int main()
 {
 
-    ll t;
+    int t;
     cin>>t;
 
     while(t--)
     {
         int n;
-        cin >> n;
+        cin>>n;
 
-        vector<int> a(n);
+        int a[n];
 
-        int sum = 0;
-        int rem1 = 0, rem2 = 0;
+        ll sum=0;
+        int k=0;
 
-        for (int i = 0; i < n; i++)
+        for(int i=0; i<n; i++)
         {
-            cin >> a[i];
-            sum += a[i];
+            cin>>a[i];
+            sum+=a[i];
 
-            if (a[i] % 3 == 1) rem1++;
-            else if (a[i] % 3 == 2) rem2++;
+            if(a[i]%3==1)
+                k=1;
+
         }
 
-
-        int rem = sum % 3;
-        if (rem == 0)
-        {
-            cout << 0 << endl;
-        }
-        else if (rem == 1)
-        {
-            if (rem1 > 0) cout << 1 << endl;
-            else cout << 2 << endl;
-        }
+        if(sum%3==0)
+            cout<<0<<endl;
+        else if(sum%3==2)
+            cout<<1<<endl;
+        else if(k==1)
+            cout<<1<<endl;
         else
-        {
-            if (rem2 > 0) cout << 1 << endl;
-            else cout << 2 << endl;
-        }
+            cout<<2<<endl;
 
     }
 
-
-    return 0;
 }
-
-
