68ec83a8436a3a9a68bdf302386b2684d95e5b06
Nayemur-Shahriar <159870495+Nayemur-Shahriar@users.noreply.github.com>
2025-03-08 22:11:09 +0600

    Add files via upload

---
+++ Week-16/day7/L - Square.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x[4], y[4];
+
+        for (int i = 0; i< 4; i++)
+        {
+            cin >> x[i] >> y[i];
+        }
+
+        sort(x, x + 4);
+        sort(y, y + 4);
+
+        int side = x[3]-x[0];
+
+        cout << (side*side) << endl;
+    }
+
+    return 0;
+}
